119416b96bc91143e40166e0782a87d0e7d56a3e
filimon4uck <filimon4uck.yura@gmail.com>
2024-08-16 03:58:56 +0200

    added calls create note

--- main.py
+++ main.py
@@ -33,12 +33,11 @@ notes_controllers = {
     "add-text": add_text,
     "add-tag": add_tag,
     "all-notes": get_notes,
-    "change-note": change_note}
-"help": get_help,
-}
+    "change-note": change_note,
+    "help": get_help}
 
 
-    def main():
+def main():
     """
     The main function that serves as the entry point for the application.
     """
@@ -52,38 +51,36 @@ notes_controllers = {
     while True:
         try:
             command = prompt.prompt(
-          "Enter a command: ", commands).strip().lower()
-           except KeyboardInterrupt:
-           print("Good bye!")
-           save_data(book)
-           break
+                "Enter a command: ", commands).strip().lower()
+        except KeyboardInterrupt:
+            print("Good bye!")
+            save_data(book)
+            break
 
-           if not command:
-           continue
+        if not command:
+            continue
 
-           if command in ["close", "exit"]:
-           print("Good bye!")
-           save_data(book)
-           break
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            save_data(book)
+            break
 
-           if command == "hello":
-           print(green("How can I help you?"))
+        if command == "hello":
+            print(green("How can I help you?"))
 
-           elif command in controllers:
-           print(controllers[command](book))
+        elif command in controllers:
+            print(controllers[command](book))
+        elif command in notes_controllers:
+            print(notes_controllers[command](notes_book))
 
-           elif command in notes_controllers:
-           print(notes_controllers[command](notes_book))
-
-           else:
-           similar_commands = suggest_command(command, commands)
-           print(danger("Invalid command."))
-           if similar_commands and similar_commands != command:
-           print("The most similar commands are")
-           for cmd in similar_commands:
-           print(f"\t'{cmd}'")
+        else:
+            similar_commands = suggest_command(command, commands)
+            print(danger("Invalid command."))
+            if similar_commands and similar_commands != command:
+                print("The most similar commands are")
+                for cmd in similar_commands:
+                    print(f"\t'{cmd}'")
 
 
-           if __name__ == "__main__":
-
-           main()
+if __name__ == "__main__":
+    main()
